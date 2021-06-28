e1c706ef4494cc5fe685a3fcc00d21f7025455f5
Dan <daniel.h.funk@gmail.com>
2021-06-28 16:54:28 -0400

    Cleaning up configuration settings.  No more 'undefined' path.

---
+++ ansible/configs/star-drive/flask/digitalocean_secrets.py
@@ -0,0 +1,21 @@
+NAME = "STAR DRIVE Database"
+VERSION = "0.1"
+
+CORS_ENABLED = True
+DEVELOPMENT = True
+TESTING = False
+MIRRORING = False
+
+API_URL = "http://143.198.34.127"
+SITE_URL = "http://143.198.34.127"
+
+SECRET_KEY = 'stardrive_impossibly_bad_key_stored_in_public_repo_dont_use_this_ouside_development_yuck!'
+
+FRONTEND_AUTH_CALLBACK = SITE_URL + "/#/session"
+FRONTEND_EMAIL_RESET = SITE_URL + "/#/reset_password/"
+
+MAIL_USERNAME = '7fb81d16128227'
+MAIL_PASSWORD = '66122eaa6db540'
+
+
+GOOGLE_MAPS_API_KEY = "AIzaSyArQNSTF3iT5lfUxGAMulquo_oAT2nMFqM"
