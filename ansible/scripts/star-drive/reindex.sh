82e993a26a0d56c7191b1d975914e0ab2d0b2cc7
Dan <daniel.h.funk@gmail.com>
2021-06-29 12:40:28 -0400

    Adding a bit to the ansible book to load a dump of the public resources from the production server so we have a larger data set to test with.

--- ansible/scripts/star-drive/reindex.sh
+++ ansible/scripts/star-drive/reindex.sh
@@ -2,8 +2,4 @@
 export FLASK_APP=~/star-drive/backend/app/__init__.py
 source ~/python-venv/bin/activate
 cd ~/star-drive/backend/
-flask cleardb
-flask db upgrade
-flask initdb
 flask initindex
-nohup flask run &
